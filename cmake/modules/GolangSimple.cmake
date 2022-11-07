85dc875b3953f89b4f91bd00af32db4175ad18a1
Andrey <and-rochev@ya.ru>
2022-11-07 11:26:10 +0400

    Update GolangSimple.cmake
    Disable go modules

--- cmake/modules/GolangSimple.cmake
+++ cmake/modules/GolangSimple.cmake
@@ -9,7 +9,7 @@ function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC)
   get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
   add_custom_target(${NAME})
   add_custom_command(TARGET ${NAME}
-                    COMMAND env GOPATH=${GOPATH} go build 
+                    COMMAND env GO111MODULE=off GOPATH=${GOPATH} go build 
                     -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
                     ${CMAKE_GO_FLAGS} ${MAIN_SRC}
                     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
